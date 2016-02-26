2c2eb2d2901e15afc66dfe399ecf9d94de19a0f9
Peter Burkholder <pburkholder@chef.io>
2016-02-26 11:25:13 -0500

    Passes TK

---
+++ resources/motd.rb
@@ -0,0 +1,11 @@
+resource_name :motd
+
+property :message, kind_of: String, name_property: true
+
+# load_current_value not needed since using underlying core property
+action :create do
+  file '/etc/motd' do
+    content "#{message}"
+    mode '0644'
+  end
+end
