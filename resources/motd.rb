2211228533f79f395327c4f7f54a7e1e428b8bf7
Peter Burkholder <pburkholder@chef.io>
2016-03-03 14:34:10 -0500

    Both examples now

--- resources/motd.rb
+++ resources/motd.rb
@@ -1,20 +1,17 @@
 require 'poise'
+#require 'compat_resource'
 
 module MotdCookbook
   class Motd < Chef::Resource
     include Poise(fused: true)
 
     resource_name :motd
-    #provides(:motd)
-
     property :message, kind_of: String, name_property: true
-    #attribute :message, kind_of: String, name_property: true
 
-    # load_current_value not needed since using underlying core property
     action :create do
       file '/etc/motd' do
-        content "#{message}"
-        #content "#{new_resource.message}"
+        # the 'custom resource' approache is just 'message', not new_resource.message.
+        content "#{new_resource.message}"
         mode '0644'
       end
     end
--- spec/unit/libraries/motd_spec.rb
+++ spec/unit/libraries/motd_spec.rb
@@ -2,13 +2,13 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'chefspec/cacher'
 require 'poise_boiler/spec_helper'
-require_relative '../../../resources/motd'
+require_relative '../../../libraries/motd'
 
 describe MotdCookbook::Motd do
   step_into(:motd)
   let(:chefspec_options) { {platform: 'ubuntu', version: '14.04', log_level: :debug} }
 
-  context 'create' do
+  context 'create_motd' do
     recipe do
       motd 'Testing'
     end
